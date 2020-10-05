3a873fef03fe94b556108c2c298aacb7a43a5dec
Loganberbs <58014408+Loganberbs@users.noreply.github.com>
2020-10-05 18:02:11 -0500

    more classes

--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Dynamic;
 
 namespace SchoolPractice
 {
@@ -7,6 +8,16 @@ namespace SchoolPractice
         public static void Main(string[] args)
         {
             // TODO: Instantiate your objects and test your exercise solutions with print statements here.
+            
+            
+            Student logan = new Student();
+
+            logan.name = "Logan Berberick";
+            logan.NumberOfCredits = 1;
+            logan.gpa = 4.0;
+
+
+
         }
     }
 }
