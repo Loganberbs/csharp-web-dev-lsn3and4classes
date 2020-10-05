926df45d544d6c2cdebaf5b551946263a048bef4
Loganberbs <58014408+Loganberbs@users.noreply.github.com>
2020-10-05 18:15:49 -0500

    Teacher.cs

---
+++ SchoolPractice/Teacher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SchoolPractice
+
+public class Teacher
+{
+    private string FirstName;
+    private string LastName;
+    private string Subject;
+    private string YearsTeaching;
+
+}
